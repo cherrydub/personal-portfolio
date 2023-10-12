c8a51ae3e81bb4545d1e2e1574abee3b32b59343
cherrydub <cherrydub@gmail.com>
2023-10-12 19:50:35 +0100

    adding a new wark for dark / light mode switching, no more whtie background on root

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Toaster, toast } from "sonner";
-import { useLocalStorage, useWindowSize } from "@uidotdev/usehooks";
-
+import { useWindowSize } from "@uidotdev/usehooks";
 import Header from "./components/Header";
 import About from "./sections/About";
 import Contact from "./sections/Contact";
@@ -11,34 +10,50 @@ import Experience from "./sections/Experience";
 import Socials from "./components/Socials";
 
 function App() {
-  const [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState("dark"); // Default to dark theme
   const [device, setDevice] = useState("mobile");
   const windoSize = useWindowSize();
 
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+    const newTheme = theme === "light" ? "dark" : "light";
+    document.documentElement.setAttribute("data-theme", newTheme);
+    setTheme(newTheme);
   };
 
   useEffect(() => {
     toast("Congrats on finding your new developer😎");
+
+    // Set the initial theme based on system preference
+    const systemPrefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+    const setSystemTheme = (event) => {
+      const newTheme = event.matches ? "dark" : "light";
+      document.documentElement.setAttribute("data-theme", newTheme);
+      setTheme(newTheme);
+    };
+
+    setSystemTheme(systemPrefersDark); // Set initial theme
+
+    // Listen for changes in system preference and update the theme
+    systemPrefersDark.addEventListener("change", setSystemTheme);
+
+    // Clean up event listener when the component unmounts
+    return () => {
+      systemPrefersDark.removeEventListener("change", setSystemTheme);
+    };
   }, []);
 
   useEffect(() => {
-    if (windoSize.width > 600) {
-      setDevice("desktop");
-    } else {
-      setDevice("mobile");
-    }
-  }, [windoSize]); // Update device state when windoSize changes
+    setDevice(windoSize.width > 600 ? "desktop" : "mobile");
+  }, [windoSize]);
 
   return (
     <div id={theme} className="App">
       <Toaster richColors />
       <Header id="top" device={device} />
-      <button id="theme-button" onClick={toggleTheme}>
-        {theme}
-      </button>
-      {/* <Body /> */}
+      <div>current theme: {theme}</div>
+      <div>device: {device}</div>
+
+      <button onClick={toggleTheme}>Toggle Theme</button>
       <About />
       <Projects />
       <Experience />
