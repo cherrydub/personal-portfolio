66ae2028ad8d71b3ffc6079050fbac9e77fb2eb5
cherrydub <cherrydub@gmail.com>
2023-10-16 16:59:58 +0100

    new page after email submission

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,24 +1,26 @@
 import React, { useState } from "react";
 import { useForm, ValidationError } from "@formspree/react";
-import { Toaster, toast } from "sonner";
+import { toast } from "sonner"; // Import toast from "sonner"
 
 const formID = import.meta.env.VITE_FORM_KEY;
 
 export default function Contact({}) {
   const [state, handleSubmit] = useForm(formID);
   const [name, setName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
 
-  const submitForm = (e) => {
-    e.preventDefault();
-    handleSubmit().then((response) => {
-      if (response.ok) {
-        toast.success(`${name}, Email sent!`);
-        setName(""); // Reset the name field
-        // You can similarly reset other form fields if needed
-      } else {
-        toast.error("Email not sent. Please try again."); // Handle errors
-      }
-    });
+  const submitForm = async (e) => {
+    e.preventDefault(); // Prevent the page from refreshing
+    const response = await handleSubmit();
+
+    if (response.ok) {
+      toast.success("Email sent!"); // Display a success toast message
+      setName(""); // Reset the name field
+      setSubmitted(true);
+      // You can similarly reset other form fields if needed
+    } else {
+      toast.error("Email not sent. Please try again."); // Handle errors with an error toast message
+    }
   };
 
   return (
@@ -45,100 +47,93 @@ export default function Contact({}) {
               </a>
             </div>
             <div className="contact-card flex flex-col justify-center items-center">
-              Get in touch easily:
-              <p></p>
-              <br />
-              <form
-                onSubmit={submitForm}
-                className="flex flex-col"
-                style={{ width: "80%" }}
-              >
-                <input
-                  className="pl-3"
-                  required
-                  id="name"
-                  placeholder="Name"
-                  type="text"
-                  name="name"
-                  value={name} // Bind the value to the state
-                  onChange={(e) => {
-                    setName(e.target.value);
-                  }}
-                />
-                <br />
+              {!submitted ? (
+                <>
+                  <span>Get in touch easily:</span>
+                  <p></p>
+                  <br />
+                  <form
+                    onSubmit={submitForm}
+                    className="flex flex-col"
+                    style={{ width: "80%" }}
+                  >
+                    <input
+                      className="pl-3"
+                      required
+                      id="name"
+                      placeholder="Name"
+                      type="text"
+                      name="name"
+                      value={name} // Bind the value to the state
+                      onChange={(e) => {
+                        setName(e.target.value);
+                      }}
+                    />
+                    <br />
 
-                {/* <label className="font-bold" htmlFor="email">
+                    {/* <label className="font-bold" htmlFor="email">
                   Email:
                 </label> */}
-                <input
-                  className="pl-3"
-                  // placeholder="your e-mail"
-                  required
-                  id="email"
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                />
-                <br />
-                {/* <label className="font-bold" htmlFor="message">
+                    <input
+                      className="pl-3"
+                      // placeholder="your e-mail"
+                      required
+                      id="email"
+                      type="email"
+                      name="email"
+                      placeholder="Email"
+                    />
+                    <br />
+                    {/* <label className="font-bold" htmlFor="message">
                   Message:
                 </label> */}
-                <textarea
-                  required
-                  placeholder="Message"
-                  id="message"
-                  name="message"
-                  className="h-40 resize-y p-2"
-                ></textarea>
+                    <textarea
+                      required
+                      placeholder="Message"
+                      id="message"
+                      name="message"
+                      className="h-40 resize-y p-2"
+                    ></textarea>
 
-                <div className="flex mt-1">
-                  <a
-                    className="text-blue-700"
-                    href="mailto:chriscoding@icloud.com"
-                  >
-                    {/* <button
-                    type="button"
-                    style={{ width: "50px" }}
-                    className="hover:bg-gray-100 flex items-center justify-center ml-auto"
-                  >
-                    <img
-                      className="items-center"
-                      title="email directly (not with form)"
-                      src="https://win98icons.alexmeub.com/icons/png/outlook_express-2.png"
-                      alt=""
+                    <div className="flex mt-1">
+                      <a
+                        className="text-blue-700"
+                        href="mailto:chriscoding@icloud.com"
+                      ></a>
+                      <button
+                        style={{
+                          width: "50px",
+                          backgroundColor: "var(--background-color)",
+                          border: "none",
+                        }}
+                        type="submit"
+                        disabled={state.submitting}
+                        className="flex items-center justify-center ml-auto"
+                      >
+                        Submit
+                      </button>
+                    </div>
+
+                    <ValidationError
+                      prefix="Name"
+                      field="name"
+                      errors={state.errors}
                     />
-                  </button> */}
-                  </a>
-                  <button
-                    style={{
-                      width: "50px",
-                      backgroundColor: "var(--background-color)",
-                      border: "none",
-                    }}
-                    type="submit"
-                    disabled={state.submitting}
-                    className="flex items-center justify-center ml-auto"
-                  >
-                    Submit
-                  </button>
-                </div>
-
-                <ValidationError
-                  prefix="Name"
-                  field="name"
-                  errors={state.errors}
-                />
-                <ValidationError
-                  prefix="Email"
-                  field="email"
-                  errors={state.errors}
-                />
-                <ValidationError
-                  prefix="Message"
-                  field="message"
-                  errors={state.errors}
-                />
-              </form>
+                    <ValidationError
+                      prefix="Email"
+                      field="email"
+                      errors={state.errors}
+                    />
+                    <ValidationError
+                      prefix="Message"
+                      field="message"
+                      errors={state.errors}
+                    />
+                  </form>
+                </>
+              ) : (
+                "Thanks for the e-mail!"
+              )}
             </div>
           </div>
         </div>
