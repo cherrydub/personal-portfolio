6877cb4688d5de9d0c6262393f0364c1fb9cc303
cherrydub <cherrydub@gmail.com>
2023-10-19 14:36:03 +0100

    more touchups added for starter section

--- src/sections/Starter.jsx
+++ src/sections/Starter.jsx
@@ -21,18 +21,35 @@ export default function Starter() {
           </span>
         </span>
       </div> */}
-      <i className="las la-terminal"></i>As an experienced IT consultant with a
-      client-centered approach, I am highly enthusiastic about programming and
-      dedicated to delivering exceptional results. Through my previous work
-      experience and personal interests, I have gained extensive expertise in
-      managing diverse systems and overseeing company structures, both hardware
-      and software. I am known for my exceptional attention to detail and strong
-      communication skills, which allow me to ensure outstanding user
-      experiences. I am particularly interested in web development roles, as I
-      am driven to create products that are user-friendly and effective. My
-      enthusiasm for programming and focus on delivering high-quality results
-      make me well-suited for any tech-oriented position. I look forward to
-      expanding my programming knowledge and experience
+      <div className="starter-info">
+        <i className="las la-terminal"></i> Web Developer based in the vibrant
+        city of London, UK, on a quest to kickstart my career in web
+        development. I recently made the exciting transition from the IT field,
+        and now I'm eagerly seeking my first role as a web developer.
+        <br />
+        <i class="las la-globe-europe"></i> I'm open to opportunities that offer
+        flexibility, whether it's in-office, a hybrid setup, or the chance to
+        work remotely across Europe. What drives me is the desire to find a role
+        that nurtures continuous learning and allows me to excel as a Software
+        Engineer.
+        <br />
+        <i class="las la-route"></i> My journey into web development has been an
+        incredible one. Fresh out of an intensive coding bootcamp, coupled with
+        a background in IT and a relentless drive for self-improvement, I'm
+        well-prepared to step into the world as a Junior Developer.
+        <br />
+        <i class="lab la-dev"></i> My development approach is deeply rooted in
+        best practices, from collaborative problem-solving through
+        pair-programming to meticulous version control. I'm well-versed in
+        diverse programming paradigms, a strong advocate of the DRY principle,
+        an implementer of SOLID design patterns, and a proficient manager of
+        CRUD operations.
+        <br />
+        <i class="lab la-connectdevelop"></i> My strength lies in adaptability,
+        proactiveness, and an unwavering commitment to continuous improvement. I
+        thrive in collaborative environments and relish the opportunity to
+        contribute to innovative projects.
+      </div>
     </div>
   );
 }
